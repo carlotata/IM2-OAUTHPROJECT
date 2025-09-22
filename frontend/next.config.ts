f25cb2ff3c9c6d852196bb8326e2714c81d7013c
Carl Aviso <avisojohn040@gmail.com>
2025-09-22 15:43:49 +0800

    added pwa set-up for nativeness: pampachoy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+   dest: "public",
+   register: true,
+   reloadOnOnline: true, 
+   disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
